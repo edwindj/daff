aa65d3bd5acf2e5bcd14a5fa330a0cf6bf649eb7
Edwin de Jonge <edwindjonge@gmail.com>
2015-02-17 14:44:46 +0100

    testing

--- R/test.R
+++ R/test.R
@@ -1,12 +1,47 @@
 
-run <- function(){
+run <- function(x=iris){
   ctx <- V8::new_context("window")
   ctx$source("./inst/daff.js")
   ctx$source(system.file("js/underscore.js", package="V8"))
   ctx$source("./inst/util.js")
-
-  ctx$call("to_table_view", iris)
+  ctx$call("to_table_view", x)
+  ctx$assign("local", x)
+  ctx$assign("local", I("to_table_view(local);"))
+  ctx$get("local")
+  #ctx$eval("to_objects(local.data)")
+  ctx$get(I("to_objects(local.data)"))
 }
 
+
+to_table_view <- function(x, name, ctx){
+  ctx$get(I("_.uniqueId.toString()"))
+  ctx$assign(name, x)
+  ttv <- paste0("to_table_view(",name,")")
+  ctx$assign(name,I(ttv))
+}
+
+to_data_frame <- function(name, ctx){
+
+}
 #
 #run()
+
+TableView <- function(ctx, df, js_name){
+  if (missing(js_name)){
+    js_name <- ctx$get(I("_.uniqueId('tv')"))
+  }
+  structure(list(
+    set_df = function(df){
+      ctx$assign(js_name, df)
+      ctx$assign(js_name, I(paste0("to_table_view(",js_name,")")))
+    },
+    get_df = function(){
+      ctx$get(I(paste0("to_objects(",js_name,".data)")))
+    }
+    )
+  , class="TableView")
+}
+
+tv <- TableView(ctx, iris)
+tv$set_df(iris)
+tv$get_df()
--- inst/util.js
+++ inst/util.js
@@ -1,14 +1,25 @@
 function to_table_view(table){
-  var view = []
+  var view = to_array(table);
+  return new daff.TableView(view);
+}
 
+function to_array(table){
+  var view = []
   if (table.length == 0){
     return view;
   }
-
   view.push(_.keys(table[0]));
-
   for (var i=0; i < table.length; i++){
     view.push(_.values(table[i]))
   }
   return view;
+}
+
+function to_objects(x){
+  var keys = x[0];
+  var result = [];
+  for (var i=1; i < x.length; i++){
+    result.push(_.object(keys, x[i]))
+  }
+  return result;
 }
