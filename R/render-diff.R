41e75031987d3e3929ec3430d791f9348a4116a1
Gregory R. Warnes <gwarnes@mdsol.com>
2017-03-20 17:16:57 -0400

    - Add title, specified by 'title' in the html header and the top of the body, as well as the current date an time,  unless `fragment==TRUE`. - Replace unicode arrow (\u2192) with html double arrow (&rArr;) in the line summary column or htmle single arrow (&rarr;) in data cells:     - since \u2192 doesn't display on some browsers, and     - differentating between arrow types make easier to search for either lines with differences or cells with differences.

--- R/render-diff.R
+++ R/render-diff.R
@@ -10,17 +10,60 @@
 #' in the browser (only if R is used interactively)
 #' @param fragment if \code{FALSE} the generated HTML will be a valid HTML document, otherwise it is a HTML fragment
 #' @param pretty if \code{TRUE} use fancy utf8 characters to make arrows prettier.
+#' @param symbol.right html code to use for arrowsq
 #' @return generated html
 #'@export
 render_diff <- function( diff
-                       , file=tempfile(fileext = ".html")
-                       , view=interactive()
-                       , fragment=FALSE
-                       , pretty=FALSE
-                       ){
+                         , file=tempfile(fileext = ".html")
+                         , view=interactive()
+                         , fragment=FALSE
+                         , pretty=FALSE
+                         , title="Daff"
+                         , ...
+)
+{
   ctx <- diff$ctx
   html <- ctx$call("render_diff", JS(diff$var_name), fragment, pretty)
+
+  if(pretty)
+  {
+    # Replace 'pretty' \u2192 Unicode arrows with html arrows, since
+    # the \u2192 Unicode character doesn't display in all browsers,
+    # including Chrome on Mac OS X
+
+    # At the start of the line, use double right arrow
+    modified.line.pattern <- '<tr class=\"modify\"><td class=\"modify\">\u2192</td>'
+    modified.line.replace <- paste0('<td class=\"modify\">&rArr;</td>')
+
+    html <- gsub(modified.line.pattern,
+                 modified.line.replace,
+                 html,
+                 fixed=TRUE)
+
+    # Anywhere else, replace with space, single right arrow, space
+    modified.cell.pattern <- '\u2192'
+    modified.cell.replace <- ' &rarr; '
+
+    html <- gsub(modified.cell.pattern,
+                 modified.cell.replace,
+                 html,
+                 fixed=TRUE)
+  }
+
+  # Add title and header if provided
+  if(!fragment && !missing(title))
+    html <- gsub("</head>\\s*<body>",
+                 paste0("<title>", title ,"</title>", "\n",
+                        "</head>", "\n",
+                        "<body>", "\n",
+                        "<h1>", "<center>", title,      "</center>", "</h1>", "\n",
+                        "<h3>", "<center>", Sys.time(), "</center>", "</h3>", "\n"
+                        ),
+                 html
+    )
+
   cat(html, file = file)
+
   if (view && file != "" && interactive()){
     viewer <- getOption("viewer")
     if (!is.null(viewer) && is.function(viewer)){
