f29e57fa9efb5b2749c242e197b79bc9dc233a0f
Edwin de Jonge <edwindjonge@gmail.com>
2017-07-06 11:51:36 +0200

    fixing travis issue

--- tests/testthat/test-print.R
+++ tests/testthat/test-print.R
@@ -4,5 +4,12 @@ test_that("print of small set works", {
   x <- data.frame(a=1, b=1)
   x_changed <- data.frame(a=1, b=100)
   diff <- diff_data(x, x_changed)
-  expect_output_file(print(diff), file = "print_small.txt", update = FALSE)
+
+  output <-
+"Daff Comparison: ‘x’ vs. ‘x_changed’
+   a b
+-> 1 1->100
+
+"
+  expect_output(print(diff), output = output, update = FALSE)
 })
